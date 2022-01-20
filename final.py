04e11012ed5d8369af5a87586848b7c868a9f7c6
fmyhre77 <56264859+fmyhre77@users.noreply.github.com>
2022-01-20 09:49:58 -0500

    adding motor code

--- final.py
+++ final.py
@@ -1,11 +1,26 @@
 import board
 import pulseio
 import adafruit_irremote
+import motor
+import pwmio
+import time
+import servo
+A1 = board.D4
+A2 = board.D5
+pwmA1 = pwmio.PWMOut(A1, frequency=50)
+pwmA2 = pwmio.PWMOut(A2, frequency=50)
+motor1 = motor.DCMotor(pwmA1, pwmA2)
+B1 = board.D7
+B2 = board.D6
+pwmB1 = pwmio.PWMOut(B1, frequency=50)
+pwmB2 = pwmio.PWMOut(B2, frequency=50)
+motor2 = motor.DCMotor(pwmB1, pwmB2)
 IR_PIN = board.D2
 pu = [1171, 1009, 3292, 1022, 2206]
 pl = [1185, 975, 1157, 3185, 2211]
 pr = [1168, 1012, 2205, 2108, 2203]
 pd = [1179, 980, 1152, 1028, 1128, 1030, 2211]
+pok = [1146, 1011, 2207, 1030, 3288]
 p1 = [1184, 977, 2239]
 p2 = [1173, 1008, 1124, 1034, 1122]
 p3 = [1185, 976, 3324]
@@ -30,12 +45,31 @@ while True:
     detected = decoder.read_pulses(pulses)
     if fuzzy_pulse_compare(pu, detected):
         print('up')
+        print("\nForwards slow")
+        motor1.throttle = 0.5
+        print("  throttle:", motor1.throttle)
+        motor2.throttle = 0.5
+        print("  throttle:", motor2.throttle)
     if fuzzy_pulse_compare(pd, detected):
         print('down')
+        print("\nBackwards")
+        motor1.throttle = -0.5
+        print("  throttle:", motor1.throttle)
+        motor2.throttle = -0.5
+        print("  throttle:", motor2.throttle)
     if fuzzy_pulse_compare(pl, detected):
         print('left')
+        motor1.throttle = 1
     if fuzzy_pulse_compare(pr, detected):
         print('right')
+        motor2.throttle = 1
+    if fuzzy_pulse_compare(pok, detected):
+        print('ok')
+        print("\nStop")
+        motor1.throttle = 0
+        print("  throttle:", motor1.throttle)
+        motor2.throttle = 0
+        print("  throttle:", motor2.throttle)
     if fuzzy_pulse_compare(p1, detected):
         print('1')
     if fuzzy_pulse_compare(p2, detected):
